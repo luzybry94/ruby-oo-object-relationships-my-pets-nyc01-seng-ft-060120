645af579bb0eee9e997bea2a8d3261231983e7cd
luzybry94 <luzybry94@gmail.com>
2020-06-04 14:49:27 -0400

    chaim code

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,46 @@
+require "pry"
+
+
 class Owner
-  # code goes here
-end
+
+  attr_reader :name, :species
+
+  @@all = []
+    
+    def initialize(name, species = "human")
+      @name = name
+      @species = species
+      @@all << self
+    end
+
+
+    def say_species
+      "I am a #{@species}."
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.count
+      @@all.count
+    end
+
+    def self.reset_all
+      @@all.clear
+    end
+
+    def cats
+      Cat.all.select do |cat| 
+        cat.owner == self
+      end
+    end
+
+    def dogs
+      Dog.all.select do |dog| 
+        dog.owner == self
+      end
+    end
+
+  end
+
